8b245775dc12b2bff03d7aed9dd96eb8430581a0
Artemy Gibson <artemygibson@gmail.com>
2023-05-04 14:59:24 -0700

    Finish Dragon

---
+++ ruby_exercises/mythical-creatures/lib/dragon.rb
@@ -0,0 +1,15 @@
+class Dragon
+    attr_reader :name, :color, :rider
+    def initialize(name, color, rider)
+        @name = name
+        @color = color
+        @rider = rider
+        @meals = 0
+    end
+    def hungry?
+        @meals < 3
+    end
+    def eat
+        @meals += 1
+    end
+end
