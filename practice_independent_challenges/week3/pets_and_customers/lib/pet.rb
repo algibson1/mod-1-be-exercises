9f494e7cd8780b9a8a0e9c6cf6f6ebbc4691f2ee
Artemy Gibson <artemygibson@gmail.com>
2023-05-09 20:21:50 -0700

    Finish pet class

--- practice_independent_challenges/week3/pets_and_customers/lib/pet.rb
+++ practice_independent_challenges/week3/pets_and_customers/lib/pet.rb
@@ -1,2 +1,15 @@
 class Pet
+    attr_reader :name, :type, :age
+    def initialize(data)
+        @name = data[:name]
+        @type = data[:type]
+        @age = data[:age]
+        @fed = false
+    end
+    def feed
+        @fed = true
+    end
+    def fed?
+        @fed
+    end
 end
