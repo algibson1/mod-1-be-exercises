711080d856a6f37bf43afb918c076c3f25201a04
Artemy Gibson <artemygibson@gmail.com>
2023-05-09 20:17:50 -0700

    Create initial files

--- practice_independent_challenges/week3/pets_and_customers/spec/customer_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/customer_spec.rb
@@ -1,42 +1,33 @@
-require './lib/customer'
-require './lib/pet'
+require "./lib/customer"
+require "./lib/pet"
 
 RSpec.describe Customer do
-  before(:each) do
-    @joel = Customer.new("Joel", 2)
-  end
+    it 'exists' do
+        joel = Customer.new("Joel", 2)
+        expect(joel).to be_a(Customer)
+    end
 
-  it "exists" do
-    expect(@joel).to be_instance_of(Customer)
-  end
+    it 'can return data on customer' do
+        joel = Customer.new("Joel", 2)
+        expect(joel.name).to eq("Joel")
+        expect(joel.id).to eq(2)
+        expect(joel.pets).to eq([])
+    end
 
-  it "has attributes" do
-    expect(@joel.name).to eq("Joel")
-    expect(@joel.id).to eq(2)
-  end
+    it 'can adopt pets' do
+        joel = Customer.new("Joel", 2)
+        samson = Pet.new({name: "Samson", type: :dog, age: 3})
+        lucy = Pet.new({name: "Lucy", type: :cat, age: 12})    
+        joel.adopt(samson)
+        joel.adopt(lucy)
+        expect(joel.pets).to eq([samson, lucy])
+    end
 
-  it "starts with no pets" do
-    expect(@joel.pets).to eq([])
-  end
-
-  it "can adopt new pets" do
-    samson = Pet.new({name: "Samson", type: :dog})
-    lucy = Pet.new({name: "Lucy", type: :cat})
-
-    @joel.adopt(samson)
-    @joel.adopt(lucy)
-    expect(@joel.pets).to eq([samson, lucy])
-  end
-
-  it "has starting outstanding balance at 0" do
-    expect(@joel.outstanding_balance).to eq(0)
-  end
-
-  it "can charge to increase oustanding balance" do
-    @joel.charge(15)
-    @joel.charge(7)
-
-    expect(@joel.outstanding_balance).to eq(22)
-  end
-
+    it 'has bills to pay' do
+        joel = Customer.new("Joel", 2)
+        expect(joel.outstanding_balance).to eq(0)
+        joel.charge(15)
+        joel.charge(7)
+        expect(joel.outstanding_balance).to eq(22)
+    end
 end
--- practice_independent_challenges/week3/pets_and_customers/spec/pet_spec.rb
+++ practice_independent_challenges/week3/pets_and_customers/spec/pet_spec.rb
@@ -1,26 +1,22 @@
-require './lib/pet'
+require "./lib/pet"
 
 RSpec.describe Pet do
-  before(:each) do
-    @samson = Pet.new({name: "Samson", type: :dog})
-  end
+    it 'exists' do
+        samson = Pet.new({name: "Samson", type: :dog, age: 3})
+        expect(samson).to be_a(Pet)
+    end
 
-  it "exists" do
-    expect(@samson).to be_instance_of(Pet)
-  end
+    it 'returns data for pets' do
+        samson = Pet.new({name: "Samson", type: :dog, age: 3})
+        expect(samson.name).to eq("Samson")
+        expect(samson.type).to eq(:dog)
+        expect(samson.age).to eq(3)
+    end
 
-  it "has attributes" do
-    expect(@samson.name).to eq("Samson")
-    expect(@samson.type).to eq(:dog)
-  end
-
-  it "is not fed by default" do
-    expect(@samson.fed?).to be(false)
-  end
-
-  it "can be fed" do
-    @samson.feed
-    expect(@samson.fed?).to be(true)
-  end
-
+    it 'feeds pets' do
+        samson = Pet.new({name: "Samson", type: :dog, age: 3})
+        expect(samson.fed?).to be false
+        samson.feed
+        expect(samson.fed?).to be true
+    end
 end
